a20aa5cea97907a205a5034c37228e1451150b2c
hs <hs970411130@gmail.com>
2026-06-06 04:41:00 +0800

    补全项目迁徙

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -128,47 +128,7 @@ typedef enum {
 
 /* Exported constants --------------------------------------------------------*/
 /* USER CODE BEGIN EC */
-#define ENABLE_FINGERPRINT      1
-#define ENABLE_RFID             1
-#define ENABLE_KEYPAD           1
-#define ENABLE_WIFI             0
 
-#define MAX_RETRY_ATTEMPTS      5
-#define LOCKOUT_DURATION_MS     30000
-#define ADMIN_PASSWORD_LENGTH   8
-#define USER_PASSWORD_LENGTH    6
-#define MAX_FINGERPRINT_COUNT   50
-#define MAX_USER_COUNT          100
-#define MAX_LOG_ENTRIES         500
-#define FLASH_LOG_ADDR          0x00000
-#define FLASH_FP_ADDR           0x40000
-#define FLASH_USER_ADDR         0x80000
-
-#define PRIO_AUTH               5
-#define PRIO_CLOUD              4
-#define PRIO_LOGGER             3
-#define PRIO_DISPLAY            2
-#define PRIO_KEYPAD             1
-#define PRIO_SUPERVISOR         0
-
-#define STACK_AUTH              2048
-#define STACK_CLOUD             1024
-#define STACK_LOGGER            1024
-#define STACK_DISPLAY           512
-#define STACK_KEYPAD            512
-#define STACK_SUPERVISOR        256
-
-#define QUEUE_AUTH_DEPTH        10
-#define QUEUE_LOG_DEPTH         20
-#define OLED_WIDTH              128
-#define OLED_HEIGHT             64
-
-#define EVENT_UNLOCK_REQ        (1 << 0)
-#define EVENT_LOCK_REQ          (1 << 1)
-#define EVENT_LOCKOUT_START     (1 << 2)
-#define EVENT_LOCKOUT_END       (1 << 3)
-#define EVENT_WIFI_CONNECTED    (1 << 4)
-#define EVENT_WIFI_LOST         (1 << 5)
 /* USER CODE END EC */
 
 /* Exported macro ------------------------------------------------------------*/
@@ -180,31 +140,7 @@ typedef enum {
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
-extern system_state_t   g_sys_state;
-extern uint32_t         g_boot_time;
-extern EventGroupHandle_t g_sys_events;
 
-extern QueueHandle_t    g_auth_queue;
-extern QueueHandle_t    g_log_queue;
-extern QueueHandle_t    g_display_queue;
-extern QueueHandle_t    g_cloud_queue;
-extern QueueHandle_t    g_cmd_queue;
-extern SemaphoreHandle_t g_flash_mutex;
-extern SemaphoreHandle_t g_user_mutex;
-extern SemaphoreHandle_t g_display_mutex;
-
-extern UART_HandleTypeDef huart1;
-extern UART_HandleTypeDef huart2;
-extern UART_HandleTypeDef huart3;
-extern SPI_HandleTypeDef  hspi1;
-extern SPI_HandleTypeDef  hspi2;
-extern I2C_HandleTypeDef  hi2c1;
-extern RTC_HandleTypeDef  hrtc;
-
-void system_set_state(system_state_t new_state);
-const char* auth_method_str(auth_method_t method);
-const char* auth_result_str(auth_result_t result);
-uint32_t get_timestamp(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
